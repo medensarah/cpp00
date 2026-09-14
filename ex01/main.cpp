15cdf64bf49670135a90b4001afe8790858b71f7
sarah <meden.sarah@gmail.com>
2026-09-14 20:02:06 +0200

    ex00 bon + ex01 main started

---
+++ ex01/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "PhoneBook.hpp"
+
+int	main()
+{
+	PhoneBook	book;
+	std::string	input;
+
+	while (true)
+	{
+		std::cout << "Enter command: (ADD, SEARCH, EXIT): ";
+		std::getline(std::cin, input);
+		if (input == "ADD")
+			book.addContact();
+		else if (input == "SEARCH")
+			book.searchContact();
+		else if (input == "EXIT")
+			break;
+		else
+			std::cout << "Unknown command." << std::endl;
+	}
+	return 0;
+}
